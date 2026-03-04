c5e22816b076adedaf790fbfa285ac6fa03ab0d4
Alexandru Barbu <rex.barbu@gmail.com>
2026-03-04 15:03:58 +0200

    Release v4.2.0

--- lib/init.sh
+++ lib/init.sh
@@ -9,7 +9,7 @@
 # arising from its use.
 
 # shellcheck disable=SC2034
-# Version: 4.1.2
+# Version: 4.2.0
 #
 # init.sh
 #
@@ -86,7 +86,7 @@ determine_VER() {
   fi
 
   # 4) Final fallback: hard-coded literal (kept for compatibility)
-  printf '%s' "4.1.2"
+  printf '%s' "4.2.0"
 }
 
 # Populate VER using the robust lookup
